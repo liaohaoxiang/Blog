ceb69c6cd06b26b518b61f60803d9728d67514ae
haoxiang <liaohaoxiang520@gmail.com>
2021-07-16 19:09:23 +0800

    fix: 百度标签

--- pages/_app.js
+++ pages/_app.js
@@ -14,8 +14,7 @@ const App = ({ Component, pageProps }) => {
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="keywords" content="liaohaoxiang,Neo's Blog,廖浩翔博客,廖浩翔"/>
-        <meta name="baidu-site-verification" content="code-epyP17tVef" />
-        <link rel="icon shortcut" href="/images/favicon.ico" type="image/x-icon"/>
+        <meta name="baidu-site-verification" content="code-H3YwJ5KaQg" />        <link rel="icon shortcut" href="/images/favicon.ico" type="image/x-icon"/>
         <meta
           name="description"
           content="廖浩翔的博客,Neo's Blog"
